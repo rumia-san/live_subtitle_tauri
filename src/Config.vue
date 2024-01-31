77f282c38abef9fcdce64b266627af8b9bb1439c
rumia <rumia-san@outlook.com>
2024-02-01 01:04:19 +0800

    show login status svg

--- src/Config.vue
+++ src/Config.vue
@@ -4,12 +4,19 @@ import { getRoomid, saveConfig } from "./components/config.js";
 import { ref, onMounted } from "vue";
 import { QRCodeGenerator } from "./3rdparty/qr.js";
 
+let show_qrcode = ref(false);
 let isLoggedIn = ref(false);
 let roomid = ref('');
 let qrcode_svg = ref('');
 
-onMounted(async () => {
+async function showLoginStatus() {
+  show_qrcode.value = false;
   isLoggedIn.value = await isLogin();
+  return isLoggedIn.value;
+}
+
+onMounted(async () => {
+  await showLoginStatus();
   roomid.value = await getRoomid();
 });
 
@@ -19,12 +26,18 @@ const save = async () => {
 }
 
 const login = async () => {
+  if (await showLoginStatus())
+    return;
   let response = await generateLoginQRCode();
   if (response) {
-    console.log(response.url);
     var svg = QRCodeGenerator.generateSVG(response.url);
     qrcode_svg.value = svg.outerHTML;
+    show_qrcode.value = true;
   }
+}
+
+const logout = async () => {
+  await showLoginStatus();
 }
 
 </script>
@@ -35,10 +48,16 @@ const login = async () => {
       <input id="room-number" v-model="roomid" placeholder="请输入房间号" />
     </div>
     <div class="settings-row">
-      <svg v-html="qrcode_svg"></svg>
+      <svg v-if="show_qrcode" v-html="qrcode_svg"></svg>
+      <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 150" width="150" height="150">
+        <rect width="150" height="150" fill="#ffffff"/>
+        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="" font-size="24" fill="black">
+          当前{{ isLoggedIn ? "已登录" : "未登录" }}
+        </text>
+      </svg>
     </div>
     <div class="settings-row">
-      <button @click="login">登录</button>
+      <button @click="login">扫码登录</button>
       <button @click="logout">登出</button>
     </div>
     <div class="settings-row">
