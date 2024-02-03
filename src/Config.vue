14d86bd07e84e30981ef04886b00aa9a48d38a1d
rumia <rumia-san@outlook.com>
2024-02-03 14:06:33 +0800

    实现扫码登录和登出

--- src/Config.vue
+++ src/Config.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { isLogin, generateLoginQRCode } from './components/bilibili_api.js';
+import { isLogin, generateLoginQRCode, pollLoginQRCode, logout } from './components/bilibili_api.js';
 import { getRoomid, saveConfig } from "./components/config.js";
+import { saveCookie } from "./components/cookie.js";
 import { ref, onMounted } from "vue";
 import { QRCodeGenerator } from "./3rdparty/qr.js";
 
@@ -8,9 +9,12 @@ let show_qrcode = ref(false);
 let isLoggedIn = ref(false);
 let roomid = ref('');
 let qrcode_svg = ref('');
+let loginPollTimer = null;
 
 async function showLoginStatus() {
   show_qrcode.value = false;
+  clearInterval(loginPollTimer); // 我们隐藏了二维码，所以停止轮询
+  loginPollTimer = null;
   isLoggedIn.value = await isLogin();
   return isLoggedIn.value;
 }
@@ -25,19 +29,46 @@ const save = async () => {
   await saveConfig(configObj);
 }
 
-const login = async () => {
+const perform_login = async () => {
   if (await showLoginStatus())
     return;
   let response = await generateLoginQRCode();
-  if (response) {
-    var svg = QRCodeGenerator.generateSVG(response.url);
-    qrcode_svg.value = svg.outerHTML;
-    show_qrcode.value = true;
+  if (response.code != 0)
+    return;
+
+  let svg = QRCodeGenerator.generateSVG(response.url);
+  qrcode_svg.value = svg.outerHTML;
+  show_qrcode.value = true;
+  if (loginPollTimer) {
+    // 要开始新的轮询了，清除旧的轮询
+    clearInterval(loginPollTimer);
   }
+  loginPollTimer = setInterval(async () => {
+    let pollResult = await pollLoginQRCode(response.qrcode_key);
+    console.log(pollResult);
+    switch (pollResult.code) {
+      case 0:
+        // 扫码成功
+        await saveCookie(pollResult.cookie);
+        await showLoginStatus();
+        break;
+      case 86038:
+        // 二维码己失效
+        await showLoginStatus();
+      default:
+        break;
+    }
+  }, 1000);
 }
 
-const logout = async () => {
-  await showLoginStatus();
+const perform_logout = async () => {
+  if (await showLoginStatus()) {
+    await logout();
+    // 在一秒后再显示一次登录状态 
+    setTimeout(async () => {
+      await showLoginStatus();
+    }, 1000);
+  }
 }
 
 </script>
@@ -50,15 +81,15 @@ const logout = async () => {
     <div class="settings-row">
       <svg v-if="show_qrcode" v-html="qrcode_svg"></svg>
       <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 150 150" width="150" height="150">
-        <rect width="150" height="150" fill="#ffffff"/>
+        <rect width="150" height="150" fill="#ffffff" />
         <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="" font-size="24" fill="black">
           当前{{ isLoggedIn ? "已登录" : "未登录" }}
         </text>
       </svg>
     </div>
     <div class="settings-row">
-      <button @click="login">扫码登录</button>
-      <button @click="logout">登出</button>
+      <button @click="perform_login">扫码登录</button>
+      <button @click="perform_logout">登出</button>
     </div>
     <div class="settings-row">
       <button @click="save">保存</button>
@@ -88,6 +119,7 @@ const logout = async () => {
   width: 100%;
   margin: 0 5px;
 }
+
 .settings-row label {
   width: 25%;
   margin: 0 5px;
